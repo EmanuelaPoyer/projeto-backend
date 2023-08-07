cbfb4f22ed2c35328280f7811c7c36fe05c9859a
EmanuelaPoyer <poyeremanuela@gmail.com>
2023-08-07 16:27:15 -0300

    adiciona a primeira versão do meu projeto

---
+++ mulheres.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const router = express.Router()
+
+const app = express()
+const porta = 3333
+
+const mulheres = [
+    {
+        nome: 'Emanuela Maria',
+        minibio: 'Desenvolvedora de Software'
+    },
+    {
+        nome: 'Pietra Poyer',
+        minibio: 'Meu amor'
+    }
+]
+
+function mostraMulheres(request, response){
+  response.json(mulheres)
+}
+
+function mostraPorta() {
+    console.log("Servidor criado e rodando na porta", porta)
+}
+
+app.use(router.get('/mulheres', mostraMulheres))
+app.listen(porta, mostraPorta)
