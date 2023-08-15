c71c406259970ab312e27a4a6a7847f39bdab787
EmanuelaPoyer <poyeremanuela@gmail.com>
2023-08-15 14:54:34 -0300

    Insere a nova versao

---
+++ mulherModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const MulherSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        requerid: true
+    },
+    citacao: {
+        type: String,
+        requerid: true,
+    },
+    minibio: {
+        type: String,
+        requerid: true,
+    }
+})
+
+module.exports = mongoose.model('diva', MulherSchema)
